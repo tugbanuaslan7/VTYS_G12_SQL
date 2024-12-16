d00f5580983d36f05ea7c22ddf5674d7edf6a6df
Tuğba Nur Aslan <tugbanur.aslan7@gmail.com>
2024-12-16 11:55:39 +0300

    fonksiyonlar güncellendi

--- fonksiyon.sql
+++ fonksiyon.sql
@@ -1,27 +1,27 @@
--- Fonksiyonun var olup olmadýðýný kontrol eder
+-- Fonksiyonun var olup olmadýðýný kontrol et
 IF OBJECT_ID ( 'EnÇokOynananUlke')
 	BEGIN
-		-- Fonksiyon varsa, siler
+		-- Fonksiyon varsa sil
 		DROP FUNCTION EnÇokOynananUlke;
 	END
 GO
 
--- Bu fonksiyon, verilen OyunId parametresine göre en çok oynanan ülkeyi döndürür.
--- Parametre olarak alýnan OyunId'ye baðlý olarak, 'Koleksiyonlar' ve 'Kullanicilar' tablolarýndaki
+-- Verilen OyunId parametresine göre en çok oynanan ülkeyi döndüren fonksiyon
+-- Parametre olarak alýnan OyunId'ye baðlý olarak 'Koleksiyonlar' ve 'Kullanicilar' tablolarýndaki
 -- kullanýcýlarýn ülkelerini gruplar ve en fazla kullanýcýya sahip olan ülkeyi belirler.
 
 CREATE FUNCTION EnÇokOynananUlke(@OyunId INT)
 RETURNS VARCHAR(50)
 AS
 	BEGIN
-		-- Fonksiyonda kullanýlacak olan 'EnÇokOynananUlke' deðiþkenini tanýmlar.
+		-- Fonksiyonda kullanýlacak olan 'EnÇokOynananUlke' deðiþkenini tanýmla
 		DECLARE @EnÇokOynananUlke VARCHAR(50)
 
-		-- 'Koleksiyonlar' tablosundaki verilen 'OyunId' için, her ülkenin kaç kez göründüðünü sayar.
-		-- 'Kullanicilar' tablosuyla iç join yaparak, kullanýcýlarýn ülkelerine göre gruplar.
-		-- Ardýndan, en çok sayýda kullanýcýya sahip olan ülkeyi seçer ve 'EnÇokOynananUlke' deðiþkenine atar.
+		-- 'Koleksiyonlar' tablosundaki verilen 'OyunId' için, her ülkenin kaç kez göründüðünü say
+		-- 'Kullanicilar' tablosuyla inner join yaparak, kullanýcýlarýn ülkelerine göre grupla
+		-- En çok sayýda kullanýcýya sahip olan ülkeyi seçer ve 'EnÇokOynananUlke' deðiþkenine ata
 		SELECT TOP 1 
-			u.Ulke = @EnÇokOynananUlke  -- Bu ülkeyi deðiþkene atar
+			u.Ulke = @EnÇokOynananUlke  -- Bu ülkeyi deðiþkene ata
 		FROM 
 			Koleksiyonlar k
 		INNER JOIN Kullanicilar u ON k.KullaniciId = u.KullaniciId
@@ -32,10 +32,10 @@ AS
 		ORDER BY 
 			COUNT(*) DESC;  -- En fazla kullanýcýya sahip ülke önce gelir
 
-		-- Hesaplanan en çok oynanan ülke geri döndürülür.
+		-- Hesaplanan en çok oynanan ülke geri döndür
 		RETURN @EnÇokOynananUlke;
 	END
-
+GO
 
 
 -- OyunID'si 1 olan oyununun en çok hangi ülkede oynandýðýný bulan sorgu
