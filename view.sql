ea7e8e3ef146fa148c97175a97e364dba6af5f95
Tuğba Nur Aslan <117925636+tugbanuaslan7@users.noreply.github.com>
2024-12-19 13:10:02 +0300

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,53 +1,129 @@
 -- VIEW'ın var olup olmadığını kontrol et
-IF OBJECT_ID('vEnÇokOynananUlkeler') IS NOT NULL
+IF OBJECT_ID('viewEnÇokOynananUlke') IS NOT NULL
 	BEGIN
-	    -- VIEW varsa sil
-		DROP VIEW vEnÇokOynananUlkeler;
+	    -- bu isimde VIEW varsa sil
+		DROP VIEW viewEnÇokOynananUlke;
 	END
 GO
 
-use boardgame
+
 
 -- oyunların hangi ülkede en çok oynandığını, o ülkeye ait oyuncu sayısını ve oyuncuların yaş gruplarını gösteren view
-CREATE VIEW EnCokOynananOyunlarByTurVeYas
+CREATE VIEW viewEnÇokOynananUlke
 AS
 SELECT 
-    o.OyunAdi,
+    o.OYUN_ASILAD,
     COUNT(*) AS ToplamOyuncu,
     CASE 
-        WHEN YEAR(GETDATE()) - YEAR(u.DogumTarihi) BETWEEN 18 AND 25 THEN '18-25'
-        WHEN YEAR(GETDATE()) - YEAR(u.DogumTarihi) BETWEEN 26 AND 35 THEN '26-35'
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 18 AND 25 THEN '18-25'
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 26 AND 35 THEN '26-35'
         ELSE '35+'
     END AS YasGrubu,
-    dbo.EnCokOynananUlke(o.OyunID) AS EnCokOynananUlke
+    dbo.fncEnÇokOynananUlke(o.OYUN_ID) AS EnCokOynananUlke
 FROM 
-    Koleksiyonlar k
-INNER JOIN Oyunlar o ON k.OyunID = o.OyunID
-INNER JOIN Kullanicilar u ON k.KullaniciId = u.KullaniciId
+    tblKoleksiyon k
+INNER JOIN tbloyun o ON k.OYUN_ID = o.OYUN_ID
+INNER JOIN tblUye u ON k.UYE_ID = u.UYE_ID
 GROUP BY 
-    o.OyunAdi,
+    o.OYUN_ASILAD,
     CASE 
-        WHEN YEAR(GETDATE()) - YEAR(u.DogumTarihi) BETWEEN 18 AND 25 THEN '18-25'
-        WHEN YEAR(GETDATE()) - YEAR(u.DogumTarihi) BETWEEN 26 AND 35 THEN '26-35'
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 18 AND 25 THEN '18-25'
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 26 AND 35 THEN '26-35'
         ELSE '35+'
     END;
 
 GO
 
+
+
+
+------
+
+CREATE VIEW viewEnÇokOynananUlke
+AS
+SELECT 
+    o.OYUN_ASILAD,
+    COUNT(*) AS ToplamOyuncu,
+    CASE 
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 18 AND 25 THEN '18-25'
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 26 AND 35 THEN '26-35'
+        ELSE '35+'
+    END AS YasGrubu,
+    enCokUlke.EnCokOynananUlke
+FROM 
+    tblKoleksiyon k
+INNER JOIN tbloyun o ON k.OYUN_ID = o.OYUN_ID
+INNER JOIN tblUye u ON k.UYE_ID = u.UYE_ID
+CROSS APPLY (SELECT dbo.fncEnÇokOynananUlke(o.OYUN_ID) AS EnCokOynananUlke) enCokUlke
+GROUP BY 
+    o.OYUN_ASILAD,
+    CASE 
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 18 AND 25 THEN '18-25'
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 26 AND 35 THEN '26-35'
+        ELSE '35+'
+    END,
+    enCokUlke.EnCokOynananUlke;
+GO
+
+
+
 -- Test: View'ı kullanarak oyun türüne, tasarımcıya ve oyuncu yaş gruplarına göre veri çeken sorgu
 SELECT 
-    o.OyunAdi,
+	o.OYUN_ASILAD,
     ec.YasGrubu,
     ec.ToplamOyuncu,
     ec.EnCokOynananUlke,
-    t.TasarimciAdi -- Tasarımcı adı
+    t.TASARIMCI_AD -- Tasarımcı adı
 FROM 
-    EnCokOynananOyunlarByTurVeYas ec
-INNER JOIN Oyunlar o ON ec.OyunAdi = o.OyunAdi
-INNER JOIN tbloyuntasarimci ot ON o.OyunID = ot.OYUN_ID -- Oyun ile tasarımcı arasındaki ilişkiyi ekledik
-INNER JOIN tbltasarimci t ON ot.TASARIMCI_ID = t.TASARIMCI_ID -- Tasarımcı bilgilerini alıyoruz
+    viewEnÇokOynananUlke ec
+INNER JOIN tbloyun o ON ec.OYUN_ASILAD = o.OYUN_ASILAD
+INNER JOIN tbloyuntasarimci ot ON o.OYUN_ID = ot.OYUN_ID -- Oyun ile tasarımcı arasındaki ilişkiyi ekledik
+INNER JOIN tblTasarimci t ON ot.TASARIMCI_ID = t.TASARIMCI_ID -- Tasarımcı bilgilerini alıyoruz
+INNER JOIN tbloyun_tur otr ON otr.OYUN_ID = o.OYUN_ID
+INNER JOIN tbltur tur ON tur.TUR_ID = otr.TUR_ID
 WHERE 
-    o.OyunTuru = 'Aksiyon'  -- Oyun türüne göre filtreleme
+    otr.TUR_ID = 1  -- Oyun türüne göre filtreleme
 ORDER BY 
-    o.OyunAdi, ec.ToplamOyuncu DESC;
+    o.OYUN_ASILAD, ec.ToplamOyuncu DESC;
+	
 
+
+
+
+
+
+
+
+CREATE VIEW viewEnÇokOynananUlke
+AS
+with encokoynanan as (
+select 
+Oyun_ıd,
+dbo.fncEnÇokOynananUlke(o.OYUN_ID) AS EnCokOynananUlke
+from tbloyun
+
+)
+SELECT 
+    o.OYUN_ASILAD,
+    COUNT(*) AS ToplamOyuncu,
+    CASE 
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 18 AND 25 THEN '18-25'
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 26 AND 35 THEN '26-35'
+        ELSE '35+'
+    END AS YasGrubu,
+	e.EnCokOynananUlke
+
+FROM 
+    tblKoleksiyon k
+INNER JOIN tbloyun o ON k.OYUN_ID = o.OYUN_ID
+INNER JOIN tblUye u ON k.UYE_ID = u.UYE_ID
+inner join encokoynanan e on o.OYUN_ID = e.OYUN_ID
+GROUP BY 
+    o.OYUN_ASILAD,
+    CASE 
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 18 AND 25 THEN '18-25'
+        WHEN YEAR(GETDATE()) - YEAR(u.DOGUMTARIHI) BETWEEN 26 AND 35 THEN '26-35'
+        ELSE '35+'
+    END;
+
+GO
